856002728075aa8024e7b858f01e3c8a44b78831
maxence <maxence.herault@gmail.com>
2020-11-28 19:35:04 +0100

    authentif(manque côté asso)

--- app/src/main/java/com/example/authentification/register.java
+++ app/src/main/java/com/example/authentification/register.java
@@ -1,14 +1,100 @@
 package com.example.authentification;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class register extends AppCompatActivity {
-
+    EditText mFullName,mEmail,mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mFullName = findViewById(R.id.editTextTextPersonName);
+        mEmail = findViewById(R.id.editTextTextEmailAddress);
+        mPassword = findViewById(R.id.editTextTextPassword);
+        mRegisterBtn = findViewById(R.id.button);
+        mLoginBtn = findViewById(R.id.textView5);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email nécessaire.");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("mot de passe nécessaire.");
+                    return;
+                }
+                if (password.length() < 7) {
+                    mPassword.setError("Minimum 7 charactères");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //enregistrer l'utilisateur dans fbase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(register.this, "Compte créé.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(register.this, "Erreur !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+
+
+            }
+        });
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),login.class));
+            }
+        });
+
+
+
+
+
+
+
     }
 }
